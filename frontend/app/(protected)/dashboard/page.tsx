35623afa4e6ef0f23cbf00a9819202de1be664e3
bharathlakkoju <lbh.lbharath@gmail.com>
2025-02-09 15:13:04 +0530

    minor changes

--- frontend/app/(protected)/dashboard/page.tsx
+++ frontend/app/(protected)/dashboard/page.tsx
@@ -1,8 +1,7 @@
 import { auth } from "@/auth";
 import DashboardPage from "@/components/dashboard-page";
 import React from "react";
-import { User } from "@prisma/client/edge";
 
 export default async function page() {
-  return <DashboardPage />;
+  return <></>
 }
